6f7ea96392a9cfa329c1c23bd06ebcb205f4ccd4
jxw7456 <jxw7456@SND2536-02>
2017-09-10 23:44:35 -0400

    hangman

---
+++ Webster - HW1_Hangman/Webster - HW1_Hangman/Webster - HW1_Hangman.cpp
@@ -0,0 +1,13 @@
+// Webster - HW1_Hangman.cpp : Defines the entry point for the console application.
+// JaJuan Webster
+// Hangman
+// Professor Maier
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
