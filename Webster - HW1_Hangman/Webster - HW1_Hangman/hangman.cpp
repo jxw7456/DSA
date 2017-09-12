484ce9618c8c600c373a64adf06ee76edbab03ef
jxw7456 <jxw7456@IGM2000-03>
2017-09-12 12:21:30 -0400

    TODO added

--- Webster - HW1_Hangman/Webster - HW1_Hangman/hangman.cpp
+++ Webster - HW1_Hangman/Webster - HW1_Hangman/hangman.cpp
@@ -10,11 +10,11 @@
 // Draws ASCII art to the console depending on the number of guesses
 void showGallows(int guessLimit)
 {
-
+	// TODO: draw ASCII art properly if guess is wrong.
 }
 
 // store proper guess letters and show the user which letters have been correctly guessed
 void showSolved(char word[], char guesses[])
 {
-
+	// TODO: store letters into proper char array
 }
