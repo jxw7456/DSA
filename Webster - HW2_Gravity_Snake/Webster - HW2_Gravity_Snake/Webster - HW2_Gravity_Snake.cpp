6f7adad19ca5f38fa6e68f3df61c30c1b4b0c839
jxw7456 <jxw7456@IGM2000-03>
2017-09-21 11:06:08 -0400

    Created HW2 Project

---
+++ Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake.cpp
@@ -0,0 +1,11 @@
+// Webster - HW2_Gravity_Snake.cpp : Defines the entry point for the console application.
+// JaJuan Webster
+// Gravity Snake
+// Professor Maier
+
+#include "stdafx.h"
+
+int main()
+{
+    return 0;
+}
