41eb6d3910f26f47a11cbe8c6e5ac68fefb37909
jxw7456 <jxw7456@IGM2000-01>
2017-09-25 21:38:11 -0400

    made some progress in main

--- Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/main.cpp
+++ Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/main.cpp
@@ -7,20 +7,48 @@
 #include "snake.h"
 #include "Box2D\Box2D.h"
 #include <iostream>
+#include <string>
 #include <crtdbg.h>
 #include <conio.h>
+using namespace std;
+
+int hits = 0;
 
 // Introduce the game to the user with instructions as to the keys to press
 int main()
 {
+	std::cout << "Welcome to Gravity Snake!\n" << endl;
+
 	// Create the physics world (b2world)
+	b2Vec2 gravity(0.0f, -10.f);	// define the gravity vector
+	b2World world(gravity);
 
 	// Create your snake (physics body b2body)
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;	// responds to forces
+	bodyDef.position.Set(0.0f, 4.0f);
+	b2Body* snake = world.CreateBody(&bodyDef);
+
+	b2PolygonShape snakeBox;
+	snakeBox.SetAsBox(1.0f, 1.0f);
+
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &snakeBox;
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+
+	snake->CreateFixture(&fixtureDef);
 
 	// Create a while loop to keep running until the user hits ESC or you get the total number of targets
-
+	/*
+	while (kbhit() != 'ESC' || hits < 2) {
 		// Check if there is a keypress (kbhit)
 		// If there is a key press then call the applyForces function
+		if (kbhit() == pressed) {
+			applyForces();
+		}
+	}
+	*/	
 
 	// The physics world will update the snake based on gravity and the other forces automatically
 
