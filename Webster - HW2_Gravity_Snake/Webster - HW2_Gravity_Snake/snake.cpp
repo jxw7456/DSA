d17f7f895c88bc7cde72629de56e3bd508449221
jxw7456 <jxw7456@IGM2000-03>
2017-09-26 11:28:39 -0400

    source files working properly

--- Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/snake.cpp
+++ Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/snake.cpp
@@ -5,25 +5,57 @@
 #include "stdafx.h"
 #include "snake.h"
 #include "Box2D\Box2D.h"
+#include <iostream>
+#include <string>
+#include <conio.h>
+using namespace std;
 
 // Updates the physics world
 void update()
 {
+
 }
 
 // Displays the position of the target and the player
-void display()
+void display(b2Vec2 target, b2Vec2 snake)
 {
+	std::cout << "Target 'X' Position: " << target.x << " Target 'Y' Position: " << target.y << endl;
+	std::cout << "Player 'X' Position: " << snake.x << " Player 'Y' Position: " << snake.y << endl;
 }
 
 // Reads the key presses and applies the forces to the player to move
 // Does NOT move player directly
-void applyForces()
+void applyForces(b2Body* body)
 {
+	char input = _getch();
+
+	switch (input)
+	{
+	case 'w':
+		std::cout << "\n 'w' is pressed";
+
+		break;
+
+	case 'a':
+		std::cout << "\n 'a' is pressed";
+
+		break;
+
+	case 's':
+		std::cout << "\n 's' is pressed";
+
+		break;
+
+	case 'd':
+		std::cout << "\n 'd' is pressed";
+
+		break;
+	}
 }
 
 // Moves the target tot a new location
 // Location will be in the range of -5.0 -> 5.0 in X and -5.0 -> 5.0 in Y
 void moveTarget(float & xPos, float & yPos)
 {
+
 }
--- Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/snake.h
+++ Webster - HW2_Gravity_Snake/Webster - HW2_Gravity_Snake/snake.h
@@ -3,11 +3,12 @@
 // Professor Maier
 
 #pragma once
+#include "Box2D\Box2D.h"
 
 void update();
 
-void display();
+void display(b2Vec2 target, b2Vec2 snake);
 
-void applyForces();
+void applyForces(b2Body* body);
 
 void moveTarget(float& xPos, float& yPos);
