0683a3f4b57f82654f3f65623a296171c6864077
JaJuan Webster <jxw7456@rit.edu>
2017-11-07 12:29:04 -0500

    moved all to .h file

--- Webster - Dynamic Queue/Webster - Dynamic Queue/Template_Queue.h
+++ Webster - Dynamic Queue/Webster - Dynamic Queue/Template_Queue.h
@@ -18,3 +18,59 @@ public:
 	bool IsEmpty(bool empty);
 };
 
+// Constructor
+template<class dataType>
+Template_Queue<dataType>::Template_Queue(dataType type)
+{
+	this->type = type;
+	std::cout << "CONSTRUCTED\n" << endl;
+}
+
+// Deconstructor
+template<class dataType>
+Template_Queue<dataType>::~Template_Queue()
+{
+	std::cout << "DESTROY!\n" << endl;
+}
+
+// a collection of pointers needed to define a constuctor that
+// allocates one space for the queue
+template<class dataType>
+void Template_Queue<dataType>::TheRuleofThree()
+{
+}
+
+// add information to the queue
+template<class dataType>
+void Template_Queue<dataType>::Push()
+{
+}
+
+// removes information of the queue from the front
+template<class dataType>
+void Template_Queue<dataType>::Pop()
+{
+}
+
+// writes out the contents of the queue
+template<class dataType>
+void Template_Queue<dataType>::Print()
+{
+	std::cout << "Queue contents: " << type << endl;
+}
+
+// returns an integer containing the amount of entries
+// (not the size of the queue, but how many entries the queue has)
+// as an integer
+template<class dataType>
+int Template_Queue<dataType>::GetSize(int size)
+{
+	return 0;
+}
+
+// informs whether or not a List contains no entries
+template<class dataType>
+bool Template_Queue<dataType>::IsEmpty(bool empty)
+{
+	return false;
+}
